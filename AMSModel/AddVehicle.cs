e395964cf9fc0a186a2aff317483a4672b537005
noumantariq377 <noumantariq377@gmail.com>
2023-05-27 20:45:48 +0500

    todaywork
    adsasdsadas

---
+++ AMSModel/AddVehicle.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMSModels
+{
+    public class AddVehicle
+    {
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Length should be in between 3 and 100")]
+        public string? FullName { get; set; }
+        [Required(AllowEmptyStrings = false)]
+        [Range(2000, 2023, ErrorMessage = "Please enter valid integer Number")]
+        public int Year { get; set; }
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(100, MinimumLength = 4, ErrorMessage = "Enter Car Manufacture")]
+        public string? Make { get; set; }
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(100, MinimumLength = 4, ErrorMessage = "Enter Model")]
+        public string? Model { get; set; }
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(100, MinimumLength = 4, ErrorMessage = "Enter VIN")]
+        public string? VIN { get; set; }
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(100, MinimumLength = 4, ErrorMessage = "Enter Current odometer reading")]
+        public string? COR { get; set; }
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(100, MinimumLength = 4, ErrorMessage = "Enter Current odometer reading")]
+        public int? CORType { get; set; }
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(1, MinimumLength = 1, ErrorMessage = "Enter Current odometer reading")]
+        public int? odometerreadingreflect { get; set; }
+        public string? OORMilegeRemarks { get; set; }
+        //Vehicle History & Condition
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(1, MinimumLength = 1, ErrorMessage = "Enter Current odometer reading")]
+        public string? VehiclePurchaseMoth { get; set; }
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(1, MinimumLength = 1, ErrorMessage = "Enter Current odometer reading")]
+        public string? VehiclePurchaseYear { get; set; }
+        public string? Vmiles { get; set; }
+        public string? mileageDistanceUnit { get; set; }
+        public string? VownershipRemarks { get; set; }
+        public string? Vaccident { get; set; }
+        public string? VaccidentRemarks { get; set; }
+        public int? VDamage { get; set; }
+        public string? VDamageRemarks { get; set; }
+        //Ownership, Title & Location
+        public int? VSeller { get; set; }
+        public string? DealerName { get; set; }
+        public int? Vtitled { get; set; }
+        public string? VtitledRemarks { get; set; }
+        public int? TitleStatus { get; set; }
+        public int? TitleState { get; set; }
+        public string? VehicleLocated { get; set; }
+        public string? Reserve { get; set; }
+        public int? AdInfo { get; set; }
+        public string? AdInfoRemarks { get; set; }
+        public byte[] Photos { get; set; }
+    }
+}
---
+++ AuctionManagementSystem/Controllers/Vehicle.cs
@@ -0,0 +1,20 @@
+﻿using AMSModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuctionManagementSystem.Controllers
+{
+    public class Vehicle : Controller
+    {
+        public IActionResult vehiclesubmission()
+        {
+            AddVehicle addVehicle = new AddVehicle();
+            return View(addVehicle);
+        }
+        [HttpPost]
+        public IActionResult vehiclesubmission(AddVehicle ad)
+        {
+            AddVehicle addVehicle = new AddVehicle();
+            return View(addVehicle);
+        }
+    }
+}
